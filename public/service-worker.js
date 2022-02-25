d53b7f9cca10abdc3b3318d7f7c7faa014f1b0c5
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2022-02-25 11:25:53 -0800

    revert to previous code for SW RSVP (b4 AT)

--- public/service-worker.js
+++ public/service-worker.js
@@ -40,7 +40,7 @@ self.addEventListener('activate', function (e) {
       // when all promises are resolved - then return
       return Promise.all(keyList.map(function (key, i) {
         if (cacheKeepList.indexOf(key) === -1) {
-          console.log('Deleteing cache: ' + keyList[i]);
+          console.log('Deleting cache: ' + keyList[i]);
           return caches.delete(keyList[i])
         }
       }));
@@ -51,19 +51,52 @@ self.addEventListener('activate', function (e) {
 
 
 //-------------- Respond with cached resources --------------
+// Respond with cached resources
 self.addEventListener('fetch', function (e) {
   console.log('fetch request : ' + e.request.url)
   e.respondWith(
-    //if resource IS cached retrieve it:
     caches.match(e.request).then(function (request) {
-      if (request) {
+      if (request) { // if cache is available, respond with cache
         console.log('responding with cache : ' + e.request.url)
         return request
-      } else { //if resource isn't cached - then get regular way:
+      } else {       // if there are no cache, try fetching request
         console.log('file is not cached, fetching : ' + e.request.url)
         return fetch(e.request)
       }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
     })
-    // return request || fetch(e.request)
   )
 })
+
+
+// ------test code w/ AT -------------------
+// self.addEventListener('fetch', function (e) {
+//   console.log('Line 55: fetch request : ' + e.request)
+//   e.respondWith(
+//     //if resource IS cached retrieve it:
+//     caches.open(CACHE_NAME).then(cache => {
+//       fetch(e.request)
+//         .then(response => {
+//           console.log('Line 61: ' + response)
+//           if (response) {
+//             console.log('LINE 62 responding with cache : ' + e.request.url)
+//             cache.put(e.request.url, response.clone())
+//           }
+//           return response
+//           // } else { //if resource isn't cached - then get regular way:
+//           //   console.log('file is not cached, fetching : ' + e.request.url)
+//           //   return fetch(request)
+//           // }
+//         })
+//         .catch(err => {
+//           console.log("This is the stinky error")
+//           return cache.match(e.request)
+//         })
+//       // return request || fetch(e.request)
+
+//     })
+//   )
+// })
+
