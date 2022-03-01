a1a808ec2e2f6b79d501eb3c10966dd01f3657db
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2022-02-28 22:40:02 -0800

    add indexed DB window const in idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,82 +1,80 @@
+// const indexedDB =
+//   window.indexedDB ||
+//   window.mozIndexedDB ||
+//   window.webkitIndexedDB ||
+//   window.msIndexedDB ||
+//   window.shimIndexedDB;
+
+// let db;
+// const request = indexedDB.open("budget", 1);
+
+// request.onupgradeneeded = ({ target }) => {
+//   let db = target.result;
+//   db.createObjectStore("pending", { autoIncrement: true });
+// };
+
+// request.onsuccess = ({ target }) => {
+//   db = target.result;
+
+//   // check if app is online before reading from db
+//   if (navigator.onLine) {
+//     checkDatabase();
+//   }
+// };
+
+// request.onerror = function (event) {
+//   console.log("Woops! " + event.target.errorCode);
+// };
+
+// function saveRecord(record) {
+//   const transaction = db.transaction(["pending"], "readwrite");
+//   const store = transaction.objectStore("pending");
+
+//   store.add(record);
+// }
+
+// function checkDatabase() {
+//   const transaction = db.transaction(["pending"], "readwrite");
+//   const store = transaction.objectStore("pending");
+//   const getAll = store.getAll();
+
+//   getAll.onsuccess = function () {
+//     if (getAll.result.length > 0) {
+//       fetch("/api/transaction/bulk", {
+//         method: "POST",
+//         body: JSON.stringify(getAll.result),
+//         headers: {
+//           Accept: "application/json, text/plain, */*",
+//           "Content-Type": "application/json"
+//         }
+//       })
+//         .then(response => {
+//           return response.json();
+//         })
+//         .then(() => {
+//           // delete records if successful
+//           const transaction = db.transaction(["pending"], "readwrite");
+//           const store = transaction.objectStore("pending");
+//           store.clear();
+//         });
+//     }
+//   };
+// }
+
+// // listen for app coming back online
+// window.addEventListener("online", checkDatabase);
+
+
+
+
+
+
 const indexedDB =
   window.indexedDB ||
   window.mozIndexedDB ||
   window.webkitIndexedDB ||
   window.msIndexedDB ||
   window.shimIndexedDB;
-
-let db;
-const request = indexedDB.open("budget", 1);
-
-request.onupgradeneeded = ({ target }) => {
-  let db = target.result;
-  db.createObjectStore("pending", { autoIncrement: true });
-};
-
-request.onsuccess = ({ target }) => {
-  db = target.result;
-
-  // check if app is online before reading from db
-  if (navigator.onLine) {
-    checkDatabase();
-  }
-};
-
-request.onerror = function (event) {
-  console.log("Woops! " + event.target.errorCode);
-};
-
-function saveRecord(record) {
-  const transaction = db.transaction(["pending"], "readwrite");
-  const store = transaction.objectStore("pending");
-
-  store.add(record);
-}
-
-function checkDatabase() {
-  const transaction = db.transaction(["pending"], "readwrite");
-  const store = transaction.objectStore("pending");
-  const getAll = store.getAll();
-
-  getAll.onsuccess = function () {
-    if (getAll.result.length > 0) {
-      fetch("/api/transaction/bulk", {
-        method: "POST",
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json"
-        }
-      })
-        .then(response => {
-          return response.json();
-        })
-        .then(() => {
-          // delete records if successful
-          const transaction = db.transaction(["pending"], "readwrite");
-          const store = transaction.objectStore("pending");
-          store.clear();
-        });
-    }
-  };
-}
-
-// listen for app coming back online
-window.addEventListener("online", checkDatabase);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -85,85 +83,85 @@ window.addEventListener("online", checkDatabase);
 //   // return;
 // }
 
-// let db;
+let db;
 
-// // establish connection / open DB
-// const request = indexedDB.open('budget_tracker', 1)
+// establish connection / open DB
+const request = indexedDB.open('budget_tracker', 1)
 
-// // To CREATE or UPDATE Object Store
-// request.onupgradeneeded = function (event) {
-//   const db = event.target.result;
-//   db.createObjectStore('updated_budget', { autoIncrement: true });
-// };
+// To CREATE or UPDATE Object Store
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore('updated_budget', { autoIncrement: true });
+};
 
-// // To handle ERRORS
-// request.onerror = function (e) {
-//   console.log("there was an error: " + e.target.errorCode)
-// };
+// To handle ERRORS
+request.onerror = function (e) {
+  console.log("there was an error: " + e.target.errorCode)
+};
 
-// // To handle SUCCESS - when DB is successfully created/upgraded
-// request.onsuccess = function (event) {
-//   db = event.target.result
+// To handle SUCCESS - when DB is successfully created/upgraded
+request.onsuccess = function (event) {
+  db = event.target.result
 
-//   // catch any error
-//   db.onerror = function (event) {
-//     console.log("error" + event.target.errorCode);
-//   }
-//   if (navigator.online) {
-//     //upload local Budget data to API
-//     uploadBudget()
-//   }
-// };
+  // catch any error
+  db.onerror = function (event) {
+    console.log("error" + event.target.errorCode);
+  }
+  if (navigator.online) {
+    //upload local Budget data to API
+    uploadBudget()
+  }
+};
 
-// function saveRecord(record) {
-//   // open new transaction w/read & write priv
-//   const transaction = db.transaction(['updated_budget'], 'readwrite');
+function saveRecord(record) {
+  // open new transaction w/read & write priv
+  const transaction = db.transaction(['updated_budget'], 'readwrite');
 
-//   // "unlock"  ObjStore
-//   const budgetObjectStore = transaction.objectStore('updated_budget');
+  // "unlock"  ObjStore
+  const budgetObjectStore = transaction.objectStore('updated_budget');
 
-//   // add to objStore
-//   budgetObjectStore.add(record);
-// }
+  // add to objStore
+  budgetObjectStore.add(record);
+}
 
-// function uploadBudget() {
-//   // open a transaction on pending db
-//   const transaction = db.transaction(['updated_budget'], 'readwrite');
+function uploadBudget() {
+  // open a transaction on pending db
+  const transaction = db.transaction(['updated_budget'], 'readwrite');
 
-//   // access the pending store
-//   const budgetObjectStore = transaction.objectStore('updated_budget');
+  // access the pending store
+  const budgetObjectStore = transaction.objectStore('updated_budget');
 
-//   //get all the records from the store
-//   const getAll = budgetObjectStore.getAll();
+  //get all the records from the store
+  const getAll = budgetObjectStore.getAll();
 
-//   getAll.onsuccess = function () {
-//     // if data in store then send to API server
-//     if (getAll.result.length > 0) {
-//       fetch('/api/transaction', {
-//         method: 'POST',
-//         body: JSON.stringify(getAll.result),
-//         headers: {
-//           Accept: 'application/json, text/plain, */*',
-//           'Content-Type': 'application/json'
-//         }
-//       })
-//         .then(res => {
-//           return res.json();
-//         })
-//         .then(serverResponse => {
-//           if (serverResponse.message) {
-//             throw new Error(serverResponse);
-//           }
+  getAll.onsuccess = function () {
+    // if data in store then send to API server
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(res => {
+          return res.json();
+        })
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
 
-//           const transaction = db.transaction(['updated_budget'], 'readwrite');
-//           const budgetObjectStore = transaction.objectStore('updated_budget');
+          const transaction = db.transaction(['updated_budget'], 'readwrite');
+          const budgetObjectStore = transaction.objectStore('updated_budget');
 
-//           // clear items in store
-//           budgetObjectStore.clear();
-//         })
-//         .catch(err => {
-//           console.log(err);
-//         });
-//     }
-//   };
-// }
+          // clear items in store
+          budgetObjectStore.clear();
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+}
--- public/service-worker.js
+++ public/service-worker.js
@@ -4,9 +4,9 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
   "/",
-  "./index.html",
-  "./css/styles.css",
-  "./js/index.js",
+  "/index.html",
+  "/css/styles.css",
+  "/js/index.js",
   "/js/idb.js",
   "/manifest.json",
   "/icons/icon-72.png",
@@ -20,104 +20,62 @@ const FILES_TO_CACHE = [
   "/icons/icon-pwa2.png"
 ]
 
+
+
+
+//-------------- Respond with cached resources --------------
+
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
+})
+
 // ------------------ Install Service Worker -----------------
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME)
       .then(function (cache) {
         console.log('Installing cache: ' + CACHE_NAME)
-        return cache.addAll(FILES_TO_CACHE);
+        return cache.addAll(FILES_TO_CACHE)
       })
-  );
-});
+  )
+})
 
 
-
-// //-------------- Respond with cached resources --------------
-// self.addEventListener('fetch', function (e) {
-//   console.log('fetch request : ' + e.request.url)
-//   e.respondWith(
-//     caches.match(e.request).then(function (request) {
-//       if (request) { // if cache is available, respond with cache
-//         console.log('responding with cache : ' + e.request.url)
-//         return request
-//       } else {       // if there are no cache, try fetching request
-//         console.log('if there are no cache, try fetching request : ' + e.request.url)
-//         return fetch(e.request)
-//       }
-
-//       // You can omit if/else for console.log & put one line below like this too.
-//       // return request || fetch(e.request)
-//     })
-//   )
-// })
-
-
-// Respond with cached resources
-self.addEventListener("fetch", function (event) {
-  // cache all get requests to /api routes
-  if (event.request.url.includes("/api/")) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return fetch(event.request)
-          .then(response => {
-            // If the response was good, clone it and store it in the cache.
-            if (response.status === 200) {
-              cache.put(event.request.url, response.clone());
-            }
-
-            return response;
-          })
-          .catch(err => {
-            // Network request failed, try to get it from the cache.
-            return cache.match(event.request);
-          });
-      }).catch(err => console.log(err))
-    );
-
-    return;
-  }
-
-  event.respondWith(
-    fetch(event.request).catch(function () {
-      return caches.match(event.request).then(function (response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get("accept").includes("text/html")) {
-          // return the cached home page for all requests for html pages
-          return caches.match("/");
-        }
-      });
-    })
-  );
-});
-
-
-// ---------- Delete outdated caches --------------
+// Delete outdated caches
 self.addEventListener('activate', function (e) {
   e.waitUntil(
-    // look for cached files with matching prefix
     caches.keys().then(function (keyList) {
-      let cacheKeepList = keyList.filter(function (key) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create keeplist
+      let cacheKeeplist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX);
       })
+      // add current cache name to keeplist
+      cacheKeeplist.push(CACHE_NAME);
 
-      // add to keeplist
-      cacheKeepList.push(CACHE_NAME);
-
-      // when all promises are resolved - then return
       return Promise.all(keyList.map(function (key, i) {
-        if (cacheKeepList.indexOf(key) === -1) {
-          console.log('Deleting cache: ' + keyList[i]);
-          return caches.delete(keyList[i])
+        if (cacheKeeplist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i]);
+          return caches.delete(keyList[i]);
         }
       }));
     })
   );
-});
-
-
-
+})
 
 // ------test code w/ AT -------------------
 // self.addEventListener('fetch', function (e) {
